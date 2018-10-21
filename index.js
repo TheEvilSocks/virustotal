f8653f3b90cfac66748bb9b89ebc0e45b1bb681a
Socks <theevilsocks@gmail.com>
2018-10-21 12:10:48 +0200

    File scanning

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const https = require('https');
 const querystring = require('querystring');
+const fs = require('fs');
 
 const Constants = require('./structures/Constants.js');
 const Dripper = require('./structures/Dripper.js');
@@ -33,7 +34,12 @@ class VirusTotal {
  	*/
 
 	constructor(apiKey, optionalSettings) {
+		if(typeof optionalSettings === "boolean")
+			optionalSettings = {private: optionalSettings};
+
  	 	optionalSettings = Object.assign(DEFAULT_SETTINGS, optionalSettings || {});
+
+ 	 	this.private = optionalSettings.private;
 
  	 	this.apiKey = apiKey;
 
@@ -63,13 +69,85 @@ class VirusTotal {
  	}
 
  	/**
+ 	 * Scan a file.
+	 * @arg {string | buffer} data The file contents to scan.
+	 * @returns {Promise<Object>} 
+ 	*/
+ 	scanFile(data){
+
+ 		return new Promise( (fulfill, reject) => {
+
+	 		if(!(data instanceof Buffer))
+	 			data = Buffer.from(data);
+	 		
+	 		if(data.length > Constants.API.MAX_PRIVATE_FILESIZE && this.private){
+	 			reject( { code: 400, message: "Filesize too big. Maximum size for PrivateAPI is 200MB." } );
+	 			return;
+	 		}
+
+	 		if(data.length > Constants.API.MAX_PUBLIC_FILESIZE && this.private == false){
+	 			reject( { code: 400, message: "Filesize too big. Maximum size for PublicAPI is 32MB." } );
+	 			return;
+	 		}
+
+	 		if(data.length > Constants.API.MAX_PUBLIC_FILESIZE && this.private){
+	 			this.getUploadUrl().then(url => {
+	 				this.apiRequest("POST", url, { file: data.toString() }).then(fulfill,reject);
+	 			});
+	 			return;
+	 		}
+
+	 		this.apiRequest("POST", Constants.API.FILES.SCAN, { file: data.toString() }).then(fulfill,reject);
+ 		});
+
+ 	}
+
+ 	/**
+ 	 * Scan a file from a path.
+	 * @arg {string} path The path the file is located at.
+	 * @returns {Promise<Object>} 
+ 	*/
+ 	scanFilePath(path){
+ 		return new Promise( (fulfill, reject) => {
+ 			fs.readFile(path, (err, data) => {
+ 				if(err){
+ 					reject(err);
+ 				}else{
+ 					this.scanFile(data).then(fulfill, reject);
+ 				}
+ 			})
+ 		});
+ 	}
+
+
+ 	/**
+ 	 * [Private API] Get the upload url to upload files larger up to 200MB.
+	 * @arg {string} path The path the file is located at.
+	 * @returns {Promise<string>} 
+ 	*/
+ 	getUploadUrl(){
+ 		return new Promise( (fulfill, reject) => {
+	 		if(!this.private){
+	 			reject({message: "Please enable the private API when initializing this module."});
+	 		}else{
+	 			this.apiRequest("GET", Constants.API.FILES.UPLOAD_URL).then(res => {
+	 				fulfill(res.upload_url);
+	 			} ,reject);
+	 		}
+ 		});
+ 	}
+
+
+
+ 	/**
  	 * Create an API request.
 	 * @arg {string} method The HTTP method to make the request with. GET POST etc.
 	 * @arg {string} path The endpoint to contact. DO NOT INCLUDE /vtapi/v2/
 	 * @arg {object} parameters Parameters to include in the request. For GET request these are URL parameters, for POST requests these are body parameters. API key is automatically included
 	 * @returns {Promise<Object>} 
  	*/
- 	apiRequest(method, path, parameters){
+ 	apiRequest(method, path, parameters = {}){
+
 		return new Promise((fulfill, reject) => {
 			this.dripper.queue(() => {
 				this._apiRequest(method, path, parameters).then(fulfill, reject);
@@ -117,17 +195,52 @@ class VirusTotal {
  				});
 
  				res.on('close', () => {
- 					if(res.statusCode == 200){
- 						fulfill( JSON.parse(data) );
- 					}else{
- 						let out = { code: res.statusCode };
+
+ 					let handleErr = () => {
+ 						var out = { code: res.statusCode, path: path, originalError: data };
+ 						switch(res.statusCode){
+ 							default:
+
+
+ 							case 403:
+ 								out.message = "Forbidden. Are you trying to use the Private API without a premium account?";
+ 							break;
+
+ 							case 404:
+ 								out.message = "Uhm, the page was not found. Could you report this to the library author?";
+ 							break;
+
+
+ 						}
+ 						
  						try{
  							out = Object.assign(out, JSON.parse(data));
  						}catch(err){}
 
  						reject( out );
  					}
+
+
+
+
+
+ 					if(res.statusCode == 200){
+ 						data = JSON.parse(data);
+
+ 						if('response_code' in data && data.response_code == 0){
+ 							handleErr();
+ 						}else{
+ 							fulfill( data );
+ 						}
+
+
+ 					}else{
+ 						handleErr();
+ 					}
+
+
  				});
+
 
  			});
 
